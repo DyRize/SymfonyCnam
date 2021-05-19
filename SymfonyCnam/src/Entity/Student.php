b46388ef9620335e9dd06d04887128b3be4c5c67
DyRize <dylan.leflour25@gmail.com>
2021-05-19 22:26:49 +0200

    Add Student and StudentType entities

---
+++ SymfonyCnam/src/Entity/Student.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\StudentRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=StudentRepository::class)
+ */
+class Student
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $arrivedAt;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $graduatedAt;
+
+    /**
+     * @ORM\OneToOne(targetEntity=User::class, cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $relatedUser;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=StudentType::class, mappedBy="students")
+     */
+    private $types;
+
+    public function __construct()
+    {
+        $this->types = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getArrivedAt(): ?\DateTimeInterface
+    {
+        return $this->arrivedAt;
+    }
+
+    public function setArrivedAt(\DateTimeInterface $arrivedAt): self
+    {
+        $this->arrivedAt = $arrivedAt;
+
+        return $this;
+    }
+
+    public function getGraduatedAt(): ?\DateTimeInterface
+    {
+        return $this->graduatedAt;
+    }
+
+    public function setGraduatedAt(?\DateTimeInterface $graduatedAt): self
+    {
+        $this->graduatedAt = $graduatedAt;
+
+        return $this;
+    }
+
+    public function getRelatedUser(): ?User
+    {
+        return $this->relatedUser;
+    }
+
+    public function setRelatedUser(User $relatedUser): self
+    {
+        $this->relatedUser = $relatedUser;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|StudentType[]
+     */
+    public function getTypes(): Collection
+    {
+        return $this->types;
+    }
+
+    public function addType(StudentType $type): self
+    {
+        if (!$this->types->contains($type)) {
+            $this->types[] = $type;
+            $type->addStudent($this);
+        }
+
+        return $this;
+    }
+
+    public function removeType(StudentType $type): self
+    {
+        if ($this->types->removeElement($type)) {
+            $type->removeStudent($this);
+        }
+
+        return $this;
+    }
+}
