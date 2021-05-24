71d1ff5955b430c2060c872ce2f7ef0d35df518f
DyRize <dylan.leflour25@gmail.com>
2021-05-24 11:42:58 +0200

    Gestion des Matières et Projets pour les étudiants

--- SymfonyCnam/src/Controller/StudentController.php
+++ SymfonyCnam/src/Controller/StudentController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Project;
 use App\Entity\Student;
+use App\Entity\Subject;
 use App\Form\StudentType;
 use App\Repository\StudentRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
@@ -39,6 +41,16 @@ class StudentController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($student);
+
+            foreach ($form->get('subjects')->getData() as $subject) {
+                $updatedSubject = $entityManager->find(Subject::class, $subject->getId());
+                $updatedSubject->addStudent($student);
+            }
+            foreach ($form->get('projects')->getData() as $project) {
+                $updatedProject = $entityManager->find(Project::class, $project->getId());
+                $updatedProject->addStudent($student);
+            }
+
             $entityManager->flush();
 
             return $this->redirectToRoute('student_index');
@@ -65,11 +77,31 @@ class StudentController extends AbstractController
      */
     public function edit(Request $request, Student $student): Response
     {
+        $entityManager = $this->getDoctrine()->getManager();
+        $subjectRepository = $entityManager->getRepository(Subject::class);
+        $projectRepository = $entityManager->getRepository(Project::class);
+
         $form = $this->createForm(StudentType::class, $student);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            foreach ($subjectRepository->findAll() as $subjectInDb) {
+                $subjectInDb->removeStudent($student);
+            }
+            foreach ($form->get('subjects')->getData() as $subject) {
+                $updatedSubject = $entityManager->find(Subject::class, $subject->getId());
+                $updatedSubject->addStudent($student);
+            }
+
+            foreach ($projectRepository->findAll() as $projectInDb) {
+                $projectInDb->removeStudent($student);
+            }
+            foreach ($form->get('projects')->getData() as $project) {
+                $updatedProject = $entityManager->find(Project::class, $project->getId());
+                $updatedProject->addStudent($student);
+            }
+
+            $entityManager->flush();
 
             return $this->redirectToRoute('student_index');
         }
--- SymfonyCnam/src/Form/StudentType.php
+++ SymfonyCnam/src/Form/StudentType.php
@@ -2,7 +2,10 @@
 
 namespace App\Form;
 
+use App\Entity\Project;
 use App\Entity\Student;
+use App\Entity\Subject;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,9 +19,18 @@ class StudentType extends AbstractType
             ->add('graduatedAt')
             ->add('relatedUser')
             ->add('promotion')
-            ->add('subjects')
-            ->add('projects')
-        ;
+            ->add('subjects', EntityType::class, [
+                'class' => Subject::class,
+                'choice_label' => 'label',
+                'expanded' => true,
+                'multiple' => true
+            ])
+            ->add('projects', EntityType::class, [
+                'class' => Project::class,
+                'choice_label' => 'label',
+                'expanded' => true,
+                'multiple' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
