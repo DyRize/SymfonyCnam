67de889c15b14e2e8f946225356c3b31f7b6f846
DyRize <dylan.leflour25@gmail.com>
2021-05-24 08:50:28 +0200

    Block access, update ToString and table and remove StudentType

--- SymfonyCnam/src/Controller/StudentController.php
+++ SymfonyCnam/src/Controller/StudentController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Student;
 use App\Form\StudentType;
 use App\Repository\StudentRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,6 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 /**
  * @Route("/student")
+ * @IsGranted("ROLE_ADMIN")
  */
 class StudentController extends AbstractController
 {
