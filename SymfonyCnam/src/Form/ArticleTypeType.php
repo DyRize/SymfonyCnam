2b72109a2c5443f43f173ae8bd27365bdef26fd9
DyRize <dylan.leflour25@gmail.com>
2021-05-31 00:07:49 +0200

    Translate to GradeType

--- SymfonyCnam/src/Form/ArticleTypeType.php
+++ SymfonyCnam/src/Form/ArticleTypeType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\ArticleType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,7 +14,9 @@ class ArticleTypeType extends AbstractType
     {
         $builder
             ->add('code')
-            ->add('label')
+            ->add('label', TextType::class, [
+                'label' => 'Libellé'
+            ])
         ;
     }
 
--- SymfonyCnam/src/Form/StudentType.php
+++ SymfonyCnam/src/Form/StudentType.php
@@ -5,8 +5,10 @@ namespace App\Form;
 use App\Entity\Project;
 use App\Entity\Student;
 use App\Entity\Subject;
+use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,18 +17,27 @@ class StudentType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('arrivedAt')
-            ->add('graduatedAt')
-            ->add('relatedUser')
+            ->add('arrivedAt', DateTimeType::class, [
+                'label' => "Date d'entrée dans la formation"
+            ])
+            ->add('graduatedAt', DateTimeType::class, [
+                'label' => "Date d'obtention du diplôme"
+            ])
+            ->add('relatedUser', EntityType::class, [
+                'class' => User::class,
+                'label' => "Utilisateur associé"
+            ])
             ->add('promotion')
             ->add('subjects', EntityType::class, [
                 'class' => Subject::class,
+                'label' => "Matières suivies",
                 'choice_label' => 'label',
                 'expanded' => true,
                 'multiple' => true
             ])
             ->add('projects', EntityType::class, [
                 'class' => Project::class,
+                'label' => "Projets associés",
                 'choice_label' => 'label',
                 'expanded' => true,
                 'multiple' => true
