e9e10b9d0615e1689501e0e4590c3c8627e5ddc1
DyRize <dylan.leflour25@gmail.com>
2021-06-02 11:26:50 +0200

    Traduction form calendar

--- SymfonyCnam/src/Form/CalendarType.php
+++ SymfonyCnam/src/Form/CalendarType.php
@@ -4,8 +4,10 @@ namespace App\Form;
 
 use App\Entity\Calendar;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ColorType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,18 +16,30 @@ class CalendarType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('title')
+            ->add('title', TextType::class, [
+                'label' => 'Titre'
+            ])
             ->add('start', DateTimeType::class, [
-                'date_widget' => 'single_text'
+                'date_widget' => 'single_text',
+                'label' => 'Date de début'
             ])
             ->add('end', DateTimeType::class, [
-                'date_widget' => 'single_text'
+                'date_widget' => 'single_text',
+                'label' => 'Date de fin'
             ])
             ->add('description')
-            ->add('all_day')
-            ->add('background_color', ColorType::class)
-            ->add('border_color', ColorType::class)
-            ->add('text_color', ColorType::class)
+            ->add('all_day', CheckboxType::class, [
+                'label' => 'Toute la journée ?'
+            ])
+            ->add('background_color', ColorType::class, [
+                'label' => 'Couleur de fond'
+            ])
+            ->add('border_color', ColorType::class, [
+                'label' => 'Couleur de bordure'
+            ])
+            ->add('text_color', ColorType::class, [
+                'label' => 'Couleur du texte'
+            ])
         ;
     }
 
